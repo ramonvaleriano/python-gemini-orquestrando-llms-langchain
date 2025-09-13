25c49a0f60168b40b817f89d46f2f309af7ebec0
Ramon Valeriano <rrvaleriano@gmail.com>
2025-09-13 00:32:26 -0300

    chore(main.py): Modificando o main.py para receber a classe

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-from src.services.lang_chain import run
+from src.services.lang_chain import LangChainService
 
-run()
+
+path_image = "src/data/exemplo_grafico.jpg"
+question = "Descreva a imagem: "
+
+if __name__ == "__main__":
+    lang_chain_service = LangChainService()
+    response = lang_chain_service.run(path_image=path_image, question=question)
+
+
+    print(response.content)
