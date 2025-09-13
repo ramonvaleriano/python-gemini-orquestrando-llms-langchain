9e99f548054b4a30a117f1d1d884fdb55100bf79
Ramon Valeriano <rrvaleriano@gmail.com>
2025-09-12 22:58:59 -0300

    chore(): Deletando arquivo que eu substituir para configurações, settings.py

--- my_keys.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-MARITACA_API_KEY = os.getenv("MARITACA_API_KEY")
--- my_models.py
+++
@@ -1,3 +0,0 @@
-GEMINI_PRO = "gemini-1.5-pro"
-GEMINI_FLASH = "gemini-1.5-flash"
-MARITACA_SABIA = "sabia-3"
