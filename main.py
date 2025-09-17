ce7bb45bb0ab7a8f2afede31495ba5c47a2af175
Ramon Valeriano <rrvaleriano@gmail.com>
2025-09-17 16:49:58 -0300

    feat(main.py e src/services/lang_chain.py): Adicionando a IA do maitaca

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from src.services.lang_chain import LangChainService
 
 
 path_image = "src/data/exemplo_grafico.jpg"
-question = "Descreva a imagem: "
+question = "Quais canais de Youtube você recomenda para que eu possa saber mais a respeito de smarpthones?"
 
 if __name__ == "__main__":
     lang_chain_service = LangChainService()
-    response = lang_chain_service.run(path_image=path_image, question=question)
+    #response = lang_chain_service.run(path_image=path_image, question=question)
+
+    response = lang_chain_service.run_maitaca(question=question)
 
 
     print(response.content)
--- src/services/lang_chain.py
+++ src/services/lang_chain.py
@@ -1,32 +1,59 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.chat_models import ChatMaritalk
 from langchain_core.messages import HumanMessage
 from langchain_core.callbacks import Callbacks
 from langchain_core.caches import BaseCache
 
-from core.settings import MODEL_1_3_FLASH, GEMINI_API_KEY
+from core.settings import (
+    MODEL_1_3_FLASH,
+    GEMINI_API_KEY,
+    MARITACA_API_KEY,
+    MARITACA_SABIA,
+)
 from src.helpers.my_helper import encode_image
 
 ChatGoogleGenerativeAI.model_rebuild()
-
-print(f"Model: {MODEL_1_3_FLASH}, Chave: {GEMINI_API_KEY}")
+ChatMaritalk.model_rebuild()
 
 class LangChainService:
     def __init__(self):
-        self.__model = MODEL_1_3_FLASH
+        self.__model_defaut = MODEL_1_3_FLASH
         self.__gemini_apy_key = GEMINI_API_KEY
+        self.__model_maritaca = MARITACA_SABIA
+        self.__maritaca_api_key = MARITACA_API_KEY
         self.llm = ChatGoogleGenerativeAI(
-            api_key=self.__gemini_apy_key, model=self.__model
+            api_key=self.__gemini_apy_key, model=self.__model_defaut
         )
 
     def run(self, path_image=None, question=None):
-        image = encode_image(path_image)
-        message = HumanMessage(
-            content=[
+        content_values = [
                 {"type": "text", "text": question},
-                {"type": "image_url", "image_url": f"data:image/jpeg;base64,{image}"},
             ]
+
+        if path_image:
+            image = encode_image(path_image)
+            content_values.append({"type": "image_url", "image_url": f"data:image/jpeg;base64,{image}"})
+
+        message = HumanMessage(
+            content=content_values
         )
 
         response = self.llm.invoke([message])
 
         return response
+
+    def run_maitaca(self, question=None):
+        self.llm = ChatMaritalk(
+            api_key=self.__maritaca_api_key, model=self.__model_maritaca
+        )
+
+        response = self.llm.invoke(question)
+
+        return response
+
+
+        
+
+
+
+        
